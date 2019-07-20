2181998a91f90ac2063424bfe97e238a1714b8fe
Lena Nordström <lena.nordstrom@medieinstitutet.se>
2019-07-20 11:35:13 +0200

    adding header component and links to home, shop and contact page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+
 import SHOP_DATA from "./shop.data.js";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-export class ShopPage extends React.Component {
+class ShopPage extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       collections: SHOP_DATA
     };
   }
+
   render() {
     const { collections } = this.state;
     return (
