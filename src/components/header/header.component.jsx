8930025cd28fd998609b26d5cbf1cc269070d9ff
Lena Nordström <lena.nordstrom@medieinstitutet.se>
2019-09-20 15:44:47 +0200

    added checkout page, checkout item component and updated cart to use selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./header.styles.scss";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-
 import { auth } from "../../firebase/firebase.utils";
+import { createStructuredSelector } from "reselect";
+
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
 
 const Header = ({ currentUser, hidden }) => (
   <header className='header'>
@@ -35,9 +39,9 @@ const Header = ({ currentUser, hidden }) => (
   </header>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,16 +1,34 @@
 import { createSelector } from "reselect";
 
-//input selector
+//input selector (takes a 'slice' out of state)
 const selectCart = state => state.cart;
 
 // output selector (uses createSelector which makes it a
-// memoized selector so that the cart icon won't have to
-// update every time state is changed)
+// memoized selector so that the cart items won't have to
+// rerender every time state is changed)
 export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems
 );
 
+// Memoized output selector for cart dropdown
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
+// Memoized output selector for cart total amount
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity * cartItem.price,
+      0
+    )
+);
+
+// Memoized output selector for cart icon quantity
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
