e15972ac55735de6c5edb6b576918b11f8eee613
Lena Nordström <lena.nordstrom@medieinstitutet.se>
2019-10-03 15:00:42 +0200

    Updating stripe inegration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_AOlh1W45MI6QQDq61JI1kfs500o1PacAA7";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful!");
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
